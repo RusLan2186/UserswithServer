1d26558e55ec8b3dbf65de2a2484a76364ad5631
Ruslan <4irus86@gmail.com>
2024-05-20 18:44:00 +0300

    change adress

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Todos from './components/Todos/Todos';
 const App: React.FC = () => {
   return (
     <div className='wrapper'>
+      <h1>Hello</h1>
       <div className='users'>
         <Todos />
       </div>
